6f59ab7d41b51824f64a4bdeaefd1c31f211eba3
Leung CHAN <CLL010@corp.ha.org.hk>
2018-02-26 09:15:08 +0800

    fix resolution

--- Examples/moonThickness.lua
+++ Examples/moonThickness.lua
@@ -1,7 +1,17 @@
-local r = 30
-local h = 2
-local t = 2
-local mt = 0.4
+local r = 36.1 -- 3 inches
+--local r = 48.8 -- 4 inches
+--local r = 61.5 -- 5 inches
+--local r = 74.2 -- 6 inches
+--local r = 86.9 -- 7 inches
+--local r = 99.6 -- 8 inches
+--local r = 112.3 -- 9 inches
+--local r = 125.0 -- 10 inches
+--local r = 137.7 -- 11 inches
+--local r = 150.4 -- 12 inches
+
+local h = 2 --bump map height
+local t = 2 --visual map thickness
+local mt = 0.4 --minimum thickness (extrude size)
 
 local heightmap = ImageSampler({
 	Filename = 'moonBumpMap.png',
@@ -25,8 +35,8 @@ local dispSampler = DisplacementSampler({
 })
 
 local lshape =  BiParametric({
-	USteps = 800,
-	WSteps = 400,
+	USteps = 720,
+	WSteps = 360,
     VertexFunction = dispSampler,
 	Thickness = -t,
 	ThicknessMap = thicknessMap,
