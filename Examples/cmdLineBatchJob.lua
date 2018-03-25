077de69436a044237b76c4a6868643d00faaeee1
Leung CHAN <CLL010@corp.ha.org.hk>
2018-03-26 00:25:09 +0800

    refine sample quality

--- Examples/cmdLineBatchJob.lua
+++ Examples/cmdLineBatchJob.lua
@@ -15,10 +15,10 @@ require "Icosahedron"
 
 function GenerateMoon(outputSize, outputName)
 	local extrudeSize = 0.4
-	local heightFactor = 2 -- bump map height factor
-	local shadowFactor = 2 -- larger is darker
+	local heightFactor = 1.5 -- bump map height factor
+	local shadowFactor = 3 -- larger is darker
 	local h = outputSize * heightFactor
-	local r = ((outputSize * 25.4) / 2) - h
+	local r = ((outputSize * 25.4) / 2) - (h / 2)
 	local t = extrudeSize * shadowFactor --visual map thickness
 
 	local heightmap = ImageSampler({
@@ -48,11 +48,11 @@ function GenerateMoon(outputSize, outputName)
 	-- 	USteps = 1440,
 	-- 	WSteps = 720,
 	local lshape =  Icosahedron({
-		RefinementLevel = 200,
+		RefinementLevel = 288,
 		VertexFunction = dispSampler,
 		Thickness = -t,
 		ThicknessMap = thicknessMap,
-		BasicThickness = -(extrudeSize + 0.5), -- a little bit thicker
+		BasicThickness = -(extrudeSize + 0.1), -- a little bit thicker
 	})
 
 	--direct output to STL
