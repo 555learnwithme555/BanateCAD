ca3adfb04c7be37a3c42ca1f4ca675836f32411e
Leung CHAN <cll010@ha.org.hk>
2018-06-27 13:16:19 +0800

    add MeshLab command for script automation

--- Examples/cmdLineBatchJob.bat
+++ Examples/cmdLineBatchJob.bat
@@ -1,3 +1,15 @@
 cd ..
 lua Examples\cmdLineBatchJob.lua
+cd Examples
+
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inches.stl -o stl\moonLamp2inches.stl
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp3inches.stl -o stl\moonLamp3inches.stl
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp4inches.stl -o stl\moonLamp4inches.stl
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inches.stl -o stl\moonLamp5inches.stl
+
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inches.stl -o stl\moonLamp2inchesY30.stl -s rotatey30.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inchesOuterOnly.stl -o stl\moonLamp2inchesY30OuterOnly.stl -s rotatey30.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inches.stl -o stl\moonLamp5inchesY45.stl -s rotatey45.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inchesOuterOnly.stl -o stl\moonLamp5inchesY45OuterOnly.stl -s rotatey45.mlx
+
 pause
