22030604393d15db62ded6a2873ec0d464d4606d
Leung CHAN <cll010@ha.org.hk>
2018-06-29 18:22:40 +0800

    fix typo

--- Examples/cmdLineBatchJob.bat
+++ Examples/cmdLineBatchJob.bat
@@ -7,9 +7,9 @@ cd Examples
 @rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp4inches.stl -o stl\moonLamp4inches.stl
 @rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inches.stl -o stl\moonLamp5inches.stl
 
-@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inches.stl -o stl\moonLamp2inchesY30.stl -s rotatey30.mlx
-@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inchesOuterOnly.stl -o stl\moonLamp2inchesY30OuterOnly.stl -s rotatey30.mlx
-@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inches.stl -o stl\moonLamp5inchesY45.stl -s rotatey45.mlx
-@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inchesOuterOnly.stl -o stl\moonLamp5inchesY45OuterOnly.stl -s rotatey45.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inches.stl -o stl\moonLamp2inchesY30.stl -s MeshLabRotateY30.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp2inchesOuterOnly.stl -o stl\moonLamp2inchesY30OuterOnly.stl -s MeshLabRotateY30.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inches.stl -o stl\moonLamp5inchesY45.stl -s MeshLabRotateY45.mlx
+@rem "C:\Program Files\VCG\MeshLab\meshlabserver" -i moonLamp5inchesOuterOnly.stl -o stl\moonLamp5inchesY45OuterOnly.stl -s MeshLabRotateY45.mlx
 
 pause
