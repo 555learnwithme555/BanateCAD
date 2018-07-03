601e73b20fafdf21ddf6ac7574bee922e4e78d70
Leung CHAN <cll010@ha.org.hk>
2018-07-03 23:29:23 +0800

    typo

--- Examples/cmdLineBatchJob.sh
+++ Examples/cmdLineBatchJob.sh
@@ -8,7 +8,7 @@ cd Examples
 # meshlabserver -i moonLamp4inches.stl -o stl/moonLamp4inches.stl
 # meshlabserver -i moonLamp5inches.stl -o stl/moonLamp5inches.stl
 
-# meshlabserver -i moonLamp2inches.stl -o stl/moonLamp2inchesY30.stl -s rotatey30.mlx
-# meshlabserver -i moonLamp2inchesOuterOnly.stl -o stl/moonLamp2inchesY30OuterOnly.stl -s rotatey30.mlx
-# meshlabserver -i moonLamp5inches.stl -o stl/moonLamp5inchesY45.stl -s rotatey45.mlx
-# meshlabserver -i moonLamp5inchesOuterOnly.stl -o stl/moonLamp5inchesY45OuterOnly.stl -s rotatey45.mlx
+# meshlabserver -i moonLamp2inches.stl -o stl/moonLamp2inchesY30.stl -s MeshLabRotateY30.mlx
+# meshlabserver -i moonLamp2inchesOuterOnly.stl -o stl/moonLamp2inchesY30OuterOnly.stl -s MeshLabRotateY30.mlx
+# meshlabserver -i moonLamp5inches.stl -o stl/moonLamp5inchesY45.stl -s MeshLabRotateY45.mlx
+# meshlabserver -i moonLamp5inchesOuterOnly.stl -o stl/moonLamp5inchesY45OuterOnly.stl -s MeshLabRotateY45.mlx
